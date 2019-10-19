657453ae89b60eefd1c661dae652cc320ceacb0d
maxxar92 <55760803+maxxar92@users.noreply.github.com>
2019-10-19 22:55:51 +0200

    only plot hosts from timespan

--- geo_stat.py
+++ geo_stat.py
@@ -88,6 +88,9 @@ def retrieve_cached_locations():
 def plot_geostat_update(timespan):
     hosts_df = host_scraper.read_hosts_from_db()
     hosts_df_timed = hosts_df[pd.notna(hosts_df["first_online_timestamp"])].copy() 
+    hosts_df_timed["datetime"] = pd.to_datetime(hosts_df_timed.first_online_timestamp * 1e9)
+    start_day = pd.Timestamp.today().date() - pd.Timedelta(days=timespan)
+    hosts_df_timed = hosts_df_timed.loc[hosts_df_timed.datetime >= start_day]
 
     fig, (ax_map,ax_linegraph) = plt.subplots(2, 1,figsize=(14, 14), gridspec_kw={'height_ratios': [6, 1],"hspace":-0.5})
     ax_map.axis('off')
@@ -123,7 +126,6 @@ def plot_geostat_update(timespan):
 
 
     ax_linegraph.yaxis.set_major_locator(MaxNLocator(integer=True))
-    hosts_df_timed["datetime"] = pd.to_datetime(hosts_df_timed.first_online_timestamp * 1e9)
     counts = hosts_df_timed.groupby([hosts_df_timed.datetime.dt.date])["datetime"].count()
     r = pd.date_range(end=pd.Timestamp.today().date(),periods=timespan)
     counts = counts.reindex(r).fillna(0.0) #fill dates without new hosts
