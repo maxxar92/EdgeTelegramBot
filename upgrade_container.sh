8a88f4ee64973486a96b72416ff8b10f81b56274
maxxar92 <55760803+maxxar92@users.noreply.github.com>
2019-11-28 22:42:27 +0100

    fix a failing Dockerfile build, add external port mapping for updates server

--- upgrade_container.sh
+++ upgrade_container.sh
@@ -2,6 +2,6 @@ docker cp edgenodebot:/home/edgenode/hosts.db .
 docker build -t edgenodebot .
 docker stop edgenodebot
 docker rm edgenodebot
-docker run -d --restart unless-stopped --name edgenodebot edgenodebot
+docker run -d --restart unless-stopped -p 0.0.0.0:5000:5000 --name edgenodebot edgenodebot
 docker logs edgenodebot
 
