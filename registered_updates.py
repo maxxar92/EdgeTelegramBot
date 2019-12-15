ed3072d3feb05bd2252d719bf7e5160bc3117637
maxxar92 <55760803+maxxar92@users.noreply.github.com>
2019-12-15 20:05:31 +0100

    change port

--- registered_updates.py
+++ registered_updates.py
@@ -59,7 +59,7 @@ def host_status(device_uuid):
     
 
 def flaskthread():
-    app.run(host='0.0.0.0', threaded=True)
+    app.run(host='0.0.0.0', port=5000, threaded=True)
 
 class UserUpdates(object):
     def register(self, telegram_id, device_token, host_uuid):
